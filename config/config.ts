01775db1078b2224e6e35d1a59fb7951fdd83c68
xuliangkai <2518907419@qq.com>
2021-03-23 18:31:20 +0800

    docs: test

--- config/config.ts
+++ config/config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     type: 'browser',
   },
   locale: {
-    // default zh-CN
+    // default zh-CN///
     default: 'zh-CN',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
